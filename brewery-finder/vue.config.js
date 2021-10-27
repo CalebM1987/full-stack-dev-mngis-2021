32f353fe9ab2ef6b66662647404d3b532d842122
CalebM1987 <caleb.mackey@gmail.com>
2021-10-27 10:59:07 -0500

    changes

--- brewery-finder/vue.config.js
+++ brewery-finder/vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.VUE_APP_PUBLIC_PATH,
-  outputDir: path.resolve(__dirname, '../services/blueprints/brewery/static/app'),
+  outputDir: path.resolve(__dirname, '../services/blueprints/brewery/static'),
 
   pluginOptions: {
     quasar: {
